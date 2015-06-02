d5443c0abd2f531c6741c0b0b7c6ce813599e033
Ken Snyder <ken@ken.net>
2015-06-02 12:41:09 +0100

    added new components

---
+++ addon/components/ui-buttons.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/ui-buttons';
+
+export default Ember.Component.extend({
+  layout: layout
+});
---
+++ addon/components/ui-radio-button.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/ui-radio-button';
+
+export default Ember.Component.extend({
+  layout: layout
+});
