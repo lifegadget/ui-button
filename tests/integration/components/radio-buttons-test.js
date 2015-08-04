e0e44d3082aa5191952c8156d0eacef52dcd1cc5
Ken Snyder <ken@ken.net>
2015-08-04 22:58:06 +0100

    added radio-buttons and checkbox-buttons

---
+++ tests/integration/components/radio-buttons-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+
+moduleForComponent('radio-buttons', 'Integration | Component | radio buttons', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{radio-buttons}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#radio-buttons}}
+      template block text
+    {{/radio-buttons}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
