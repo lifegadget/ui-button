956a1c82274e127f4a49b7d12d92a392e315a4ac
Ken Snyder <ken@ken.net>
2016-10-05 23:48:21 +0100

    Released 2.1.2

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,27 +1,11 @@
 import Ember from 'ember';
+import navHandler from 'ui-navigation/mixins/nav-handler';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(navHandler, {
 
   isRepressed: false,
   toggledEnablement: false,
   isIndexPage: Ember.computed.equal('currentPath', 'index'),
   notIndexPage: Ember.computed.not('isIndexPage'),
 
-  actions: {
-    navChange: function(args) {
-      const [context] = args;
-      const {routeTo, linkTo} = context.getProperties('routeTo', 'linkTo');
-      if(linkTo) {
-        window.open(linkTo);
-      }
-      if (routeTo) {
-        if (this.transitionToRoute) {
-
-          this.transitionToRoute(routeTo);
-        } else {
-          Ember.debug('component needs to have transitionToRoute passed in');
-        }
-      }
-    }
-  }
 });
